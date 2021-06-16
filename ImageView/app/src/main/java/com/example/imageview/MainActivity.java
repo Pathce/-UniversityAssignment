40ce3be64f0d3cd811657b207390cd8d92b4049f
Pathce <asfgzx@naver.com>
2021-06-16 16:02:33 +0900

    기말시험 대비

--- ImageView/app/src/main/java/com/example/imageview/MainActivity.java
+++ ImageView/app/src/main/java/com/example/imageview/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.imageview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView1, imageView2, imageView3, imageView4;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        imageView3 = (ImageView) findViewById(R.id.imageView3);
+        imageView4 = (ImageView) findViewById(R.id.imageView4);
+
+        imageView1.setImageResource(R.drawable.ic_android);
+        imageView2.setImageResource(R.drawable.ic_apple);
+
+        Thread mThread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    URL url = new URL("https://yt3.ggpht.com/ytc/AAUvwnjwtR-RhpxTpdQqX-7SSEmr_jSCOirVBlgQWVv9Vg=s900-c-k-c0x00ffffff-no-rj");
+
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoInput(true);
+                    conn.connect();
+
+                    InputStream is = conn.getInputStream();
+                    bitmap = BitmapFactory.decodeStream(is);
+
+                }catch(MalformedURLException e){
+                    e.printStackTrace();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        mThread.start();
+
+        try{
+            mThread.join();
+            imageView3.setImageBitmap(bitmap);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
